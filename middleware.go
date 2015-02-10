724b4b61bdfea2e1b5df52c64e5cf28ce9f157b1
uzybzk <5226228+uzybzk@users.noreply.github.com>
2015-02-10 14:20:00 +0300

    Add middleware for logging and CORS

---
+++ middleware.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+)
+
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        
+        next.ServeHTTP(w, r)
+        
+        log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+    }
+}
+
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+        
+        next.ServeHTTP(w, r)
+    }
+}
